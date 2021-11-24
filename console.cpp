8ccd426f3bb9026c2f8baf0140d486f55fa9bce7
a13579and2468 <a13579and2468@a13579and2468-X555LD>
2021-11-24 22:12:39 +0800

    complete

--- console.cpp
+++ console.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <fstream>
+#include <sstream>
+#include <array>
 
 using namespace std;
 using namespace boost::asio;
@@ -12,29 +15,30 @@ io_service ioservice;
 
 void parsearg()
 {
-    string arg = string(getenv("QUERY_STRING"));
-    for(int i=0;i<5;i++)
-    {
-        h[i] = arg.substr(arg.find("h"+to_string(i)));
-        h[i] = h[i].substr(3,h[i].find("&")-3);
-        p[i] = arg.substr(arg.find("p"+to_string(i)));
-        p[i] = p[i].substr(3,p[i].find("&")-3);
-        f[i] = arg.substr(arg.find("f"+to_string(i)));
-        f[i] = f[i].substr(3,f[i].find("&")-3);
-    }
+  string arg = string(getenv("QUERY_STRING"));
+  for (int i = 0; i < 5; i++)
+  {
+    h[i] = arg.substr(arg.find("h" + to_string(i)));
+    h[i] = h[i].substr(3, h[i].find("&") - 3);
+    p[i] = arg.substr(arg.find("p" + to_string(i)));
+    p[i] = p[i].substr(3, p[i].find("&") - 3);
+    f[i] = arg.substr(arg.find("f" + to_string(i)));
+    f[i] = f[i].substr(3, f[i].find("&") - 3);
+  }
 }
 
 void print_html_template()
 {
-    string heads = "";
-    string tds = "";
-    for(int i=0;i<5;i++)
-    {
-        if(h[i]=="")break;
-        heads += "<th scope=\"col\">"+h[i]+":"+p[i]+"</th>\n";
-        tds += "<td><pre id=\"s"+to_string(i)+"\" class=\"mb-0\"></pre></td>\n";
-    }
-    printf(R"(
+  string heads = "";
+  string tds = "";
+  for (int i = 0; i < 5; i++)
+  {
+    if (h[i] == "")
+      break;
+    heads += "<th scope=\"col\">" + h[i] + ":" + p[i] + "</th>\n";
+    tds += "<td><pre id=\"s" + to_string(i) + "\" class=\"mb-0\"></pre></td>\n";
+  }
+  printf(R"(
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -86,116 +90,168 @@ void print_html_template()
     </table>
   </body>
 </html>
-)",heads.c_str(),tds.c_str());
+)",
+         heads.c_str(), tds.c_str());
 }
 
-void escape(std::string& data) {
-    std::string buffer;
-    buffer.reserve(data.size());
-    for(size_t pos = 0; pos != data.size(); ++pos) {
-        switch(data[pos]) {
-            case '\n':  buffer.append("&NewLine;");       break;
-            case '&':  buffer.append("&amp;");       break;
-            case '\"': buffer.append("&quot;");      break;
-            case '\'': buffer.append("&apos;");      break;
-            case '<':  buffer.append("&lt;");        break;
-            case '>':  buffer.append("&gt;");        break;
-            default:   buffer.append(&data[pos], 1); break;
-        }
-    }
-    data.swap(buffer);
-}
-
-void output_shell(int session,std::string content)
+void escape(std::string &data)
 {
-    escape(content);
-    printf("<script>document.getElementById('s%d').innerHTML += '%s';</script>",session,content.c_str());
-    fflush(stdout);
+  std::string buffer;
+  buffer.reserve(data.size());
+  for (size_t pos = 0; pos != data.size(); ++pos)
+  {
+    switch (data[pos])
+    {
+    case '\n':
+      buffer.append("&NewLine;");
+      break;
+    case '\r':
+      buffer.append("");
+      break;
+    case '&':
+      buffer.append("&amp;");
+      break;
+    case '\"':
+      buffer.append("&quot;");
+      break;
+    case '\'':
+      buffer.append("&apos;");
+      break;
+    case '<':
+      buffer.append("&lt;");
+      break;
+    case '>':
+      buffer.append("&gt;");
+      break;
+    default:
+      buffer.append(&data[pos], 1);
+      break;
+    }
+  }
+  data.swap(buffer);
 }
-void output_command(int session,std::string content){
-    escape(content);
-    printf("<script>document.getElementById('s%d').innerHTML += '<b>%s</b>';</script>",session,content.c_str());
-    fflush(stdout);
+
+void output_shell(int session, std::string content)
+{
+  escape(content);
+  printf("<script>document.getElementById('s%d').innerHTML += '%s';</script>", session, content.c_str());
+  fflush(stdout);
+}
+void output_command(int session, std::string content)
+{
+  escape(content);
+  printf("<script>document.getElementById('s%d').innerHTML += '<b>%s</b>';</script>", session, content.c_str());
+  fflush(stdout);
 }
 
 class session
-  : public std::enable_shared_from_this<session>
+    : public std::enable_shared_from_this<session>
 {
 public:
-    session(std::string h,std::string p,std::string f,int i)
-      : h(h),p(p),f(f),s_number(i)
+  session(std::string h, std::string p, std::string f, int i)
+      : h(h), p(p), f(f), s_number(i)
+  {
+    std::ifstream file("./test_case/" + f);
+    if (file)
     {
+      file_stream << file.rdbuf();
+      file.close();
     }
+  }
 
-    void start()
-    {
-        auto self(shared_from_this());
-        ip::tcp::resolver::query q{h, p};
-        _resolv.async_resolve(q, 
-        [this, self](const boost::system::error_code &ec,ip::tcp::resolver::iterator it)
-        {
-            if (!ec)do_connect(it);
-        }
-        );
-    }
+  void start()
+  {
+    auto self(shared_from_this());
+    ip::tcp::resolver::query q{h, p};
+    _resolv.async_resolve(q,
+                          [this, self](const boost::system::error_code &ec, ip::tcp::resolver::iterator it)
+                          {
+                            if (!ec)
+                              do_connect(it);
+                          });
+  }
 
 private:
-    void do_connect(ip::tcp::resolver::iterator it)
-    {
-        auto self(shared_from_this());
-        _socket.async_connect(*it, 
-        [this, self](const boost::system::error_code &ec)
-        {
-            if (!ec)
-            {
-                do_read();
-            }
-        });
-    }
+  void do_connect(ip::tcp::resolver::iterator it)
+  {
+    auto self(shared_from_this());
+    _socket.async_connect(*it,
+                          [this, self](const boost::system::error_code &ec)
+                          {
+                            if (!ec)
+                            {
+                              do_read();
+                            }
+                          });
+  }
 
-    void do_read()
-    {
-        auto self(shared_from_this());
-        boost::asio::async_read_until(_socket,sockiobuf,"% ",
-        [this, self](boost::system::error_code ec, std::size_t bytes_transferred)
-        {
-            if (!ec)
-            {
-                char * buffer = new char [bytes_transferred+1];
-                iostream(&sockiobuf).read(buffer,bytes_transferred);
-                buffer[bytes_transferred] = '\x00';
-                output_shell(s_number,std::string(buffer));
-                do_write();
-            }
-        });
-    }
+  void do_read()
+  {
+    auto self(shared_from_this());
+    _socket.async_read_some(boost::asio::buffer(onebyte),
+                            [this, self](boost::system::error_code ec, std::size_t bytes_transferred)
+                            {
+                              if (!ec)
+                              {
+                                cerr << onebyte[0];
+                                output_shell(s_number, string(1, onebyte[0]));
+                                if (onebyte[0] == '%')
+                                {
+                                  percent = true;
+                                }
+                                else if (percent && onebyte[0] == ' ')
+                                {
+                                  percent = false;
+                                  do_write();
+                                }
+                                do_read();
+                              }
+                            });
+  }
 
-    void do_write()
-    {
-        
-    }
+  void do_write()
+  {
+    auto self(shared_from_this());
+    std::string line;
+    getline(file_stream, line);
+    output_command(s_number, line + "\n");
+    cerr << line;
+    boost::asio::async_write(_socket, boost::asio::buffer((line + "\n").c_str(), line.length() + 1),
+                             [this, self](boost::system::error_code ec, std::size_t bytes_transferred)
+                             {
+                               if (!ec)
+                               {
+                                 //do_read();
+                               }
+                             });
+  }
 
-    boost::asio::streambuf sockiobuf;
-    std::string h;
-    std::string p;
-    std::string f;
-    int s_number;
-    ip::tcp::resolver _resolv{ioservice};
-    ip::tcp::socket _socket{ioservice};
+  boost::asio::streambuf sockiobuf;
+  std::string h;
+  std::string p;
+  std::string f;
+  int s_number;
+  bool percent = false;
+  std::array<char, 1> onebyte;
+  ip::tcp::resolver _resolv{ioservice};
+  ip::tcp::socket _socket{ioservice};
+  std::stringstream file_stream;
 };
 
-int main(){
-    printf("Content-type: text/html\r\n\r\n");
-    
-    parsearg();
-    print_html_template();
+int main()
+{
+  printf("Content-type: text/html\r\n\r\n");
 
-    for(int i=0;i<5;i++)
-    {
-        if(h[i]=="")break;
-        std::make_shared<session>(h[i],p[i],f[i],i)->start();
-    }
+  parsearg();
+  print_html_template();
 
-    ioservice.run();
-    return 0;
+  for (int i = 0; i < 5; i++)
+  {
+    if (h[i] == "")
+      break;
+    std::make_shared<session>(h[i], p[i], f[i], i)->start();
+  }
+
+  ioservice.run();
+  return 0;
 }
